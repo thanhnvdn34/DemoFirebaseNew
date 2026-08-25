a517ad781ceb38beaf5038f7b060413f0b811355
Thanh <vanthanh.nguyendn34@gmail.com>
2026-08-26 00:41:26 +0700

    Update variant

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
         applicationId = "com.thanhnv.demofirebase"
         minSdk = 28
         targetSdk = 37
+        versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
